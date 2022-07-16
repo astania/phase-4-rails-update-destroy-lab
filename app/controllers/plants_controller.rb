3a56745fd94a33aef1e68464420ba0027555f2f8
+ <+>
2022-07-16 14:22:57 -0400

    commit tree 27a433e5e63785daa263284df49771e9fa073502

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,6 @@
 class PlantsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :return_error_message
   # GET /plants
   def index
     plants = Plant.all
@@ -8,7 +9,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant 
     render json: plant
   end
 
@@ -18,9 +19,33 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # UPDATE /plants/:id
+
+  def update 
+    plant = find_plant 
+    plant.update(plant_params)
+    render json: plant
+  end 
+
+  #DELETE /plants/:id
+
+  def destroy 
+    plant = find_plant
+    plant.destroy
+    head :no_content
+  end 
+
   private
+
+  def find_plant 
+    Plant.find(params[:id])
+  end 
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def return_error_message 
+    render json: {error: "plant not found"}
+  end 
 end
